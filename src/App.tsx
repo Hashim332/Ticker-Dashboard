03f22aa9ac1448a76b46437f8c6219389f4012b9
Hashim332 <ppyhr5@nottingham.ac.uk>
2025-04-24 17:59:06 +0100

    UI update

--- src/App.tsx
+++ src/App.tsx
@@ -66,7 +66,7 @@ function App() {
 
       {photographer && (
         <div className="fixed bottom-0 right-0 p-4 text-right text-white drop-shadow-md z-10">
-          <p>By: {photographer}</p>
+          <p>Photo by: {photographer}</p>
         </div>
       )}
     </div>
