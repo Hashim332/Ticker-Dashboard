3be50d3726b2b90270796d85b0056d4e47ffbf75
Hashim332 <ppyhr5@nottingham.ac.uk>
2025-04-25 14:07:40 +0100

    mobile layout update

--- src/components/StockCard.tsx
+++ src/components/StockCard.tsx
@@ -9,19 +9,22 @@ export default function StockCard({ stock, removeCard }: StockCardProps) {
   const currentPrice = stock.c;
   const changeAmount = stock.d;
   const changePercentage = stock.dp;
-
   const isPositive = changeAmount > 0;
   const emoji = isPositive ? "📈" : "📉";
 
   return (
     <div
-      className={`rounded-xl shadow-md p-3 md:p-6 md:px-10 font-sans text-gray-800 flex flex-col justify-center
+      className={`
+        w-full           /* take 100% width on small screens */
+        rounded-xl shadow-md p-3 md:p-6 md:px-10 
+        font-sans text-gray-800 flex flex-col justify-center
         ${
           isPositive
             ? "bg-gradient-to-br from-green-50 via-white to-green-100"
             : "bg-gradient-to-br from-red-50 via-white to-red-100"
         }
-        border border-white`}
+        border border-white
+      `}
     >
       <div className="flex flex-row justify-between items-center">
         <div className="text-base md:text-lg font-bold text-gray-900 mb-1 md:mb-2 flex items-center gap-1 md:gap-2">
@@ -29,7 +32,7 @@ export default function StockCard({ stock, removeCard }: StockCardProps) {
         </div>
         <button
           onClick={() => removeCard(stock.ticker)}
-          className="w-6 h-6 md:w-7 md:h-7 flex items-center justify-center rounded-full border border-gray-500 text-gray-500 hover:bg-red-500 hover:text-white transition-colors duration-200 hover:cursor-pointer text-xs md:text-sm" // Text size is text-xs on small, md:text-sm on medium+
+          className="w-6 h-6 md:w-7 md:h-7 flex items-center justify-center rounded-full border border-gray-500 text-gray-500 hover:bg-red-500 hover:text-white transition-colors duration-200 hover:cursor-pointer text-xs md:text-sm"
         >
           x
         </button>
